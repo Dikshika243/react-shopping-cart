aec55f95500ae61dfd2b8e4f10035b3f56e40c54
Dikshika Ahir <dikshika.c.ahir@tretainfotech.com>
2021-03-01 17:46:15 +0530

    modal

--- src/components/products.js
+++ src/components/products.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
-import formatCurrency from '../util'
+import Fade from "react-reveal/Fade"
+import Modal from "react-modal"
+import Zoom from "react-reveal/Zoom"
+// import formatCurrency from '../util'
 
 export default class products extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      product:null
+    }
+  }
+  openModal=(product)=>{
+    this.setState({product});
+  }
+  closeModal=()=>{
+    this.setState({product:null})
+  }
     render() {
+      const {product}= this.state;
         return (
             <div>
+              <Fade bottom cascade>
                    <ul className="products">
               {this.props.products.map((product) => (
                 <li key={product._id}>
                   <div className="product">
                     <a
-                      href={"#" + product._id}
+                      href={"#" + product._id }
                       onClick={() => this.openModal(product)}
                     >
                       <img src={product.image} alt={product.title}></img>
@@ -29,6 +46,45 @@ export default class products extends Component {
                 </li>
               ))}
             </ul>
+            </Fade>
+            {product &&
+             <Modal isOpen={ true}
+             onRequestClose={this.closeModal}> 
+                <Zoom>
+                  <button className="close-modal" onClick={this.closeModal}>x</button>
+                  <div className="product-details">
+                    <img src={product.image} alt={product.title}></img>
+                      <div className="product-details-description">
+                        <p>
+            <strong>{product.title}</strong>
+                        </p>
+                        <p>
+                          {product.description}
+                        </p>
+                        <p>
+                        Available sizes : {" "}
+                          {product.availableSizes.map(x=>(
+                            <span>{" "} <button className="button">{x}</button></span>
+                          ))}
+                        </p>
+                        <div className="produt-price">
+                            <div>
+                              $ {product.price}
+                              <button className="button primary"
+                              onClick={()=>{
+                              this.props.addToCart(product);
+                              this.closeModal();}}>
+                                Add to Cart
+
+                              </button>
+                            </div>
+                        </div>
+                      </div>
+                    
+                  </div>
+                  <div>Modal</div>
+                </Zoom>
+              </Modal>}
             </div>
         )
     }
